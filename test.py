8c1cf6f641c7b587d31a6cb4c89b4a1237e2d9fd
sacca97 <tommaso.sacchetti@gmail.com>
2023-03-15 09:35:47 +0100

    Fix

--- test.py
+++ test.py
@@ -4,10 +4,12 @@ from bs4 import BeautifulSoup
 dir = os.listdir("norules/relevance/")
 for file in dir:
     with open("norules/relevance/" + file, "r") as f:
+        if file[-4:] != "html":
+            continue
         soup = BeautifulSoup(f, "html.parser")
         out = [
             l.get_text().strip()[:50]
-            for l in soup.find_all("div", class_="col-10")
+            for l in soup.find_all("div", class_="card-text")
             if len(l.get_text().strip()) > 0
         ]
         with open(file[:-4] + "txt", "w") as ff:
